037a357e82045d144075f98b35e448a0e4f316fd
Franz-Benjamin Mocnik <mail@franz-benjamin.net>
2017-07-18 14:01:29 +0200

    FaceCoordinates.distanceTo introduced

--- src/main/java/com/giscience/utils/geogrid/geometry/FaceCoordinates.java
+++ src/main/java/com/giscience/utils/geogrid/geometry/FaceCoordinates.java
@@ -44,6 +44,9 @@ public class FaceCoordinates {
         return this._y;
     }
     
+    public Double distanceTo(FaceCoordinates c) {
+        return Math.sqrt(Math.pow(this._x - c.getX(), 2) + Math.pow(this._y - c.getY(), 2));
+    }
     @Override
     public String toString() {
         return String.format("face %d x %f y %f", this._face, this._x, this._y);
