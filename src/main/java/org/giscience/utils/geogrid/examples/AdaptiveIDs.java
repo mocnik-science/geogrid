65d8c4d007ab608b4d63146518fbd9f4b3cb5d8e
Franz-Benjamin Mocnik <mail@franz-benjamin.net>
2017-12-20 02:45:05 +0100

    AdaptiveIDs introduced

---
+++ src/main/java/org/giscience/utils/geogrid/examples/AdaptiveIDs.java
@@ -0,0 +1,27 @@
+package org.giscience.utils.geogrid.examples;
+
+import org.giscience.utils.geogrid.cells.GridCell;
+import org.giscience.utils.geogrid.cells.GridCellIDType;
+import org.giscience.utils.geogrid.cells.GridCellMetaData;
+import org.giscience.utils.geogrid.geometry.FaceCoordinates;
+import org.giscience.utils.geogrid.geometry.GeoCoordinates;
+import org.giscience.utils.geogrid.grids.ISEA3H;
+import org.giscience.utils.geogrid.projections.ISEAProjection;
+
+import java.util.*;
+
+/**
+ *
+ * @author Franz-Benjamin Mocnik
+ */
+public class AdaptiveIDs {
+    public static void main(String[] args) throws Exception {
+
+        for (int resolution = 1; resolution <= 30; resolution++) {
+            System.out.println("====== " + resolution);
+            System.out.println(GridCellMetaData.getInstance().numberOfDecimalPlaces(resolution, GridCellIDType.NON_ADAPTIVE));
+            System.out.println(GridCellMetaData.getInstance().numberOfDecimalPlaces(resolution, GridCellIDType.ADAPTIVE_UNIQUE));
+            System.out.println(GridCellMetaData.getInstance().numberOfDecimalPlaces(resolution, GridCellIDType.ADAPTIVE_1_PERCENT));
+        }
+    }
+}
