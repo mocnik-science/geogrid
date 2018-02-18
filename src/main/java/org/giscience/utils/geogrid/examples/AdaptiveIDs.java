125919c7fcb90009279111b728a67ac0750f4976
Franz-Benjamin Mocnik <mail@franz-benjamin.net>
2018-02-18 14:45:35 +0100

    Examples improved

--- src/main/java/org/giscience/utils/geogrid/examples/AdaptiveIDs.java
+++ src/main/java/org/giscience/utils/geogrid/examples/AdaptiveIDs.java
@@ -11,13 +11,13 @@ import org.giscience.utils.geogrid.projections.ISEAProjection;
 import java.util.*;
 
 /**
+ * Computes the number of decimal places needed at a given resolution, depending on the type of cell ID.
  *
  * @author Franz-Benjamin Mocnik
  */
 public class AdaptiveIDs {
     public static void main(String[] args) throws Exception {
-
-        for (int resolution = 1; resolution <= 30; resolution++) {
+        for (int resolution = 1; resolution <= 22; resolution++) {
             System.out.println("====== " + resolution);
             System.out.println(GridCellMetaData.getInstance().numberOfDecimalPlaces(resolution, GridCellIDType.NON_ADAPTIVE));
             System.out.println(GridCellMetaData.getInstance().numberOfDecimalPlaces(resolution, GridCellIDType.ADAPTIVE_UNIQUE));
--- src/main/java/org/giscience/utils/geogrid/examples/Example.java
+++ src/main/java/org/giscience/utils/geogrid/examples/Example.java
@@ -1,25 +1,28 @@
 package org.giscience.utils.geogrid.examples;
 
+import org.giscience.utils.geogrid.cells.GridCellMetaData;
 import org.giscience.utils.geogrid.geometry.FaceCoordinates;
 import org.giscience.utils.geogrid.geometry.GeoCoordinates;
-import org.giscience.utils.geogrid.geometry.GridCell;
+import org.giscience.utils.geogrid.cells.GridCell;
 import org.giscience.utils.geogrid.grids.ISEA3H;
+import org.giscience.utils.geogrid.cells.GridCellIDType;
 import org.giscience.utils.geogrid.projections.ISEAProjection;
 
-import java.util.Collection;
+import java.util.*;
 
 /**
+ * Demonstrates the use of the projection from the sphere to the icosahedron and back, as well as the use of the ISEA3H
+ * grid.
  *
  * @author Franz-Benjamin Mocnik
  */
 public class Example {
     public static void main(String[] args) throws Exception {
-
         // PROJECTION
         ISEAProjection p = new ISEAProjection();
 
         // project coordinates from the sphere to the icosahedron and back
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 1; i++) {
             GeoCoordinates c = new GeoCoordinates(Math.random() * 180 - 90, Math.random() * 360);
             FaceCoordinates c2 = p.sphereToIcosahedron(c);
             GeoCoordinates c3 = p.icosahedronToSphere(c2);
@@ -45,54 +48,12 @@ public class Example {
         System.out.println("------");
 
         // determine cell for geographic coordinates
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 1; i++) {
             double lat = Math.random() * 180 - 90;
             double lon = Math.random() * 360;
             GridCell c = g.cellForLocation(lat, lon);
-            System.out.format("lat %f - lon %f%n", lat, lon);
             System.out.println(c);
             System.out.println("------");
         }
-        // alternatively, cellForLocation accepts a GeoCoordinates object
-        // the method cellForCentroid can be used to compute the grid cell for a jts Geometry
     }
 }
-
-
-/*
-
-icosahedron
-north and south points on the edge midpoints (symmetric around the equator)
-hexagons, aperture 3 as the smallest possible aligned hexagon aperture
-inverse ISEA projection
-DGGS point at the center of the corresponding planar cell region
--> ISEA Aperture 3 Hexagon DGGS (ISEA3H)
-
-
-        // -9 223 372 036 854 775 808
-        // -92 23372036 854775808
-        // -92 12345678 123456789
-        // 90.372,036 lat
-        // 180.775,808 lon
-        // resolution: 111 km = 1 <=> 111 m = 0.001 <=> 0,111 m = 0.000,001
-
-40,075 km / 5 faces = 8,015 km / face
-
-8,015 km * 2/3 * (1/sqrt(3))^15 = 1.4106 m
-8,015 km * 2/3 * (1/sqrt(3))^16 = 0.8144 m
-8,015 km * 2/3 * (1/sqrt(3))^17 = 0.4702 m
-8,015 km * 2/3 * (1/sqrt(3))^18 = 0.2715 m
-8,015 km * 2/3 * (1/sqrt(3))^19 = 0.1567 m
-8,015 km * 2/3 * (1/sqrt(3))^20 = 0.0905 m
-
-resolution between 0 and 18
-
-erdoberfläche: 510,100,000 km^2
--> radius: 6371.2218793249 km
--> umfang: 40031.5677009551 km
-
-Documentation
-
-* result: only between -180 and 180 lon
-
-*/
